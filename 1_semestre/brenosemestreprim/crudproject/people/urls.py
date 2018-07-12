15df876396b4c37f105a87e3868d138fcb1c37a1
Marcelo <marcelo.scientist@hotmail.com>
2018-07-12 20:27:12 -0300

    urls and some basic templates

--- 1_semestre/brenosemestreprim/crudproject/people/urls.py
+++ 1_semestre/brenosemestreprim/crudproject/people/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('',views.PersonList.as_view, name='person_list'),
-#    path('view/<int:pk>', views.PersonView.as_view, name='person_view'),
-    path('new', views.PersonCreate.as_view, name='person_new'),
-    path('view/<int:pk>', views.PersonView.as_view, name='person_view'),
-    path('edit/<int:pk>', views.PersonUpdate.as_view, name='person_edit'),
-    path('delete/<int:pk>', views.PersonDelete.as_view, name='person_delete'),
+    path('',views.PersonList.as_view(), name='person_list'),
+    path('view/<int:pk>', views.PersonView.as_view(), name='person_view'),
+    path('new', views.PersonCreate.as_view(), name='person_new'),
+    path('view/<int:pk>', views.PersonView.as_view(), name='person_view'),
+    path('edit/<int:pk>', views.PersonUpdate.as_view(), name='person_edit'),
+    path('delete/<int:pk>', views.PersonDelete.as_view(), name='person_delete'),
 ]
