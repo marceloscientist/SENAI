a956473d545c29a395373382c7873cb549da76a0
Marcelo <marcelo.scientist@hotmail.com>
2018-07-15 16:30:26 -0300

    fixing button create

--- 1_semestre/brenosemestreprim/crudproject/crudpersonalinfo/urls.py
+++ 1_semestre/brenosemestreprim/crudproject/crudpersonalinfo/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('people/', include('people.urls'))
+    path('people/', include('people.urls')),
 ]
